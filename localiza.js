ef9612b31e955b34f813aa1ceda3274cd2010d44
bronx96 <gmarc251@gmail.com>
2023-06-05 16:22:27 -0300

    adição do ejs

--- localiza.js
+++ localiza.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+//selecionadno o motor de html para o express exibir, ele basicamente pega uma rquivo html e torna legivel para o nosso servidor
+app.set('view engine', 'ejs');
 
+//acessando o banco de dados atras de um import do arquivo db.js aonde o vinculo foi criado
 const db = require('./models/db')
 
 app. get("/", async (req, res) =>{
-    res.send("ola mundo");
+    res.render("LocalRegistro");
 })
 app.listen(8080, () => {
     console,console.log(" servidor na porta 8080: http://localhost:8080");
