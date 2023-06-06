e13ecb84fc03e3d049c5c52ff43c80533cb8dfb1
bronx96 <gmarc251@gmail.com>
2023-06-06 02:23:52 -0300

    modificando html com ejs

--- localiza.js
+++ localiza.js
@@ -7,8 +7,19 @@ app.set('view engine', 'ejs');
 //acessando o banco de dados atras de um import do arquivo db.js aonde o vinculo foi criado
 const db = require('./models/db')
 
+app.use(express.static('public'))
+
 app. get("/", async (req, res) =>{
-    res.render("LocalRegistro");
+    res.render("pesquisa", {
+    });
+})
+app. get("/local/:id/:nome", async (req, res) =>{
+    var id = req.params.id;
+    var nome = req.params.nome;
+    res.render("pesquisa", {
+        id: id,
+        nome: nome
+    });
 })
 app.listen(8080, () => {
     console,console.log(" servidor na porta 8080: http://localhost:8080");
